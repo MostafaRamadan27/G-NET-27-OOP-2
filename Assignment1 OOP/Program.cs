31e1aca36ee1231ba4bb541adba7c7703c1617de
MostafaRamadan27 <mostafaramadan200018@gmail.com>
2026-02-21 14:39:00 +0200

    Q1

--- Assignment1 OOP/Program.cs
+++ Assignment1 OOP/Program.cs
@@ -6,7 +6,13 @@
         {
             #region Part 01 : Theoretical Questions
             #region Q1 
-
+            //A)
+            //Field Must be Private 
+            // in method must put validation before withdraw 
+            // B)
+            // 1- make field private   2- put validation before withdraw in method
+            // c) 
+            // because user can put invalid data 
             #endregion
 
 
