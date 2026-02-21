e7333619aa0f46ae89ddf3a29e43714a11263342
MostafaRamadan27 <mostafaramadan200018@gmail.com>
2026-02-21 14:27:43 +0200

    Add project files.

---
+++ Assignment1 OOP/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Assignment1_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Part 01 : Theoretical Questions
+            #region Q1 
+
+            #endregion
+
+
+
+            #endregion
+        }
+    }
+}
